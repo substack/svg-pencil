bd9f46895249fd612292a8a0f48854c564aeb452
James Halliday <mail@substack.net>
2014-01-01 23:09:06 -0800

    save-as

--- example/main.js
+++ example/main.js
@@ -1,3 +1,20 @@
 var pencil = require('../');
 var p = pencil();
 p.appendTo('#viewport');
+
+var link = document.querySelector('#download')
+link.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    saveAs('pretty.svg');
+});
+
+function saveAs (filename) {
+    var link = document.createElement('a');
+    var div = document.createElement('div');
+    div.appendChild(p.element.cloneNode(true));
+    
+    var data = btoa(div.innerHTML);
+    link.setAttribute('download', filename);
+    link.setAttribute('href', 'data:image/svg;base64,' + data);
+    link.click();
+}
